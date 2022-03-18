b40eab7ac190833f303a1a1dea8a72411e2d39ff
werachon88892 <94419942+werachon88892@users.noreply.github.com>
2022-03-18 14:32:16 +0700

    Add files via upload

---
+++ public/data/brand.js
@@ -0,0 +1,20 @@
+module.exports = {
+  data: {
+    company_uat_en: 'Company Name',
+    company_uat_cn: '公司名',
+    company_live_en: 'Green Entrepreneurship',
+    company_live_cn: '首页-世界环保创业基金会',
+    company_phone_uat: "+(4122) 5186 162",
+    company_phone_live: "+(4122) 5186 162",
+    company_email_uat: "system@green-entrepreneurship.cc",
+    company_email_live: "system@green-entrepreneurship.cc",
+    address_uat_en: 'Rue du Rhone 14, 1204 Geneva',
+    address_uat_cn: '日内瓦1204区杜罗纳街14号',
+    address_live_en: 'Rue du Rhone 14, 1204 GenevaWcompany',
+    address_live_cn: '日内瓦1204区杜罗纳街14号Wcompany',
+    logo_uat_en: '/images/logo_live_en.png',
+    logo_uat_cn: '/images/logo_live_cn.png',
+    logo_live_en: '/images/logo_live_en.png',
+    logo_live_cn: '/images/logo_live_cn.png',
+  }
+};
